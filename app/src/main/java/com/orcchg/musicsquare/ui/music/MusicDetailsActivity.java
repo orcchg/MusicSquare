eac5752377065431e01583926696c0b0cea2c442
Alov Maxim <alovmax@yandex.ru>
2016-04-04 16:53:24 +0300

    Better page-proofs

--- app/src/main/java/com/orcchg/musicsquare/ui/music/MusicDetailsActivity.java
+++ app/src/main/java/com/orcchg/musicsquare/ui/music/MusicDetailsActivity.java
@@ -2,6 +2,7 @@ package com.orcchg.musicsquare.ui.music;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
@@ -9,19 +10,27 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.orcchg.musicsquare.R;
 import com.orcchg.musicsquare.data.model.Musician;
 import com.orcchg.musicsquare.ui.base.BaseActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import fr.castorflex.android.circularprogressbar.CircularProgressBar;
 
 public class MusicDetailsActivity extends BaseActivity<MusicDetailsPresenter> implements MusicDetailsMvpView {
 
     @Bind(R.id.collapsing_toolbar) CollapsingToolbarLayout mCollapsingToolbar;
     @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.pb_loading) CircularProgressBar mProgressBar;
+    @Bind(R.id.iv_cover) ImageView mCoverImageView;
     @Bind(R.id.tv_desription) TextView mDescriptionTextView;
     @Bind(R.id.tv_link) TextView mLinkTextView;
 
@@ -59,6 +68,22 @@ public class MusicDetailsActivity extends BaseActivity<MusicDetailsPresenter> im
         mToolbar.setTitle(musician.getName());
         mDescriptionTextView.setText(musician.getDescription());
         mLinkTextView.setText(musician.getLink());
+
+        Glide.with(this)
+            .load(musician.getCovers().get(Musician.COVER_BIG))
+            .listener(new RequestListener<String, GlideDrawable>() {
+                @Override
+                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    mProgressBar.setVisibility(View.GONE);
+                    return false;
+                }
+            })
+            .into(mCoverImageView);
     }
 
     /* Internals */
@@ -69,11 +94,8 @@ public class MusicDetailsActivity extends BaseActivity<MusicDetailsPresenter> im
     }
 
     private void initToolbar() {
-        @ColorInt int color = getResources().getColor(android.R.color.white);
-        mCollapsingToolbar.setExpandedTitleColor(color);
-        mCollapsingToolbar.setCollapsedTitleTextColor(color);
-        mToolbar.setTitleTextColor(color);
-        mToolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp));
+        mCollapsingToolbar.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        mCollapsingToolbar.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
