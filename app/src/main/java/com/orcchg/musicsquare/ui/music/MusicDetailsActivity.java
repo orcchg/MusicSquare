6aaf6835b0e7827ec35d2490e26b1fb5da1f83b1
Alov Maxim <alovmax@yandex.ru>
2016-04-04 23:22:15 +0300

    Using TOP_CROP image transform to fit large image into app-bar-layout area. I assume that the head of a musician is mostly close to the top edge or an image, thus it is better to crop the bottom and let the head fit the borders.

--- app/src/main/java/com/orcchg/musicsquare/ui/music/MusicDetailsActivity.java
+++ app/src/main/java/com/orcchg/musicsquare/ui/music/MusicDetailsActivity.java
@@ -2,6 +2,7 @@ package com.orcchg.musicsquare.ui.music;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
@@ -18,12 +19,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.orcchg.musicsquare.R;
 import com.orcchg.musicsquare.data.model.Musician;
 import com.orcchg.musicsquare.ui.base.BaseActivity;
+import com.orcchg.musicsquare.util.ImageTransform;
 import com.orcchg.musicsquare.util.MusicianUtils;
 import com.orcchg.musicsquare.util.ViewUtility;
 
@@ -110,19 +111,21 @@ public class MusicDetailsActivity extends BaseActivity<MusicDetailsPresenter> im
 
         Glide.with(this)
             .load(musician.getCovers().get(Musician.COVER_BIG))
-            .listener(new RequestListener<String, GlideDrawable>() {
+            .asBitmap()
+            .listener(new RequestListener<String, Bitmap>() {
                 @Override
-                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
                     // TODO: handle error
                     return false;
                 }
 
                 @Override
-                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
                     stopProgressAnimation();
                     return false;
                 }
             })
+            .transform(ImageTransform.create(this, ImageTransform.TOP_CROP))
             .into(mCoverImageView);
 
         setGrade(musician);
